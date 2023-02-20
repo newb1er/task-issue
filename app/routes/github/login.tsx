613c06417735c3cdad6d07dd0b9b8cdf9e0ccd2b
newb1er <taiwanmichael429@gmail.com>
2023-02-20 15:50:41 +0800

    feat: redirect to localhost after login

--- app/routes/github/login.tsx
+++ app/routes/github/login.tsx
@@ -1,10 +1,13 @@
 const baseUrl = "https://github.com/login/oauth/authorize";
 const clientId = process.env.GITHUB_CLIENT_ID;
+const siteUrl = `${process.env.SITE_URL}/github/callback`;
 
 export default function Login() {
+    console.log(siteUrl);
+
     return (
         <>
-            <a href={`${baseUrl}?client_id=${clientId}`}>
+            <a href={`${baseUrl}?client_id=${clientId}&redirect_uri=${siteUrl}`}>
                 <button>Login</button>
             </a>
         </>
